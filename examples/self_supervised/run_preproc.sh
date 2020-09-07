f7002f86b2c3135f77969a2ba1393b4193d38396
ALABI Jesujoba Oluwadara <alabijesujoba@gmail.com>
2020-09-07 20:20:54 +0200

    Update run_preproc.sh

--- examples/self_supervised/run_preproc.sh
+++ examples/self_supervised/run_preproc.sh
@@ -1,1 +1,9 @@
 ##
+python preprocess.py  --destdir Comparable/data-bin/en_ne_sp6k/ \
+  --source-lang src \
+  --target-lang tgt \
+  --trainpref /path/to/corpus.{src|tgt}  \  
+  --validpref /path/to/valid.{src|tgt}  \
+  --dataset-impl raw \
+  --joined-dictionary \
+  --workers 60  
