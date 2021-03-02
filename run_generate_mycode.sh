268495e3fa175fb71ac2e115a1a46b9381e8d090
damyana79 <damyana@gmx.net>
2021-03-02 20:01:12 +0100

    run script with smalltest

--- run_generate_mycode.sh
+++ run_generate_mycode.sh
@@ -29,4 +29,25 @@ CUDA_VISIBLE_DEVICES=4,5 python fairseq_cli/generate.py $testdir --path $model \
 }
 
 
-call_basic_model "en" "de"
+call_basic_model_smalltest() {
+src="$1"
+tgt="$2"
+
+model="${top}/checkpoints/basic-transf/checkpoint_best.pt"
+testdir="${top}/data/data-bin-32k-red-lazy-new-shorter-minitest-5st-renamed"
+outdir="/raid/data/daga01/fairseq_out_test"
+mkdir -p "$outdir"
+#--cpu \
+CUDA_VISIBLE_DEVICES=7 python fairseq_cli/generate.py $testdir --path $model \
+ --batch-size 8 --beam 2 --nbest 12 \
+ --dataset-impl lazy \
+ --print-alignment \
+ --tokenizer moses -s $src -t $tgt \
+ --remove-bpe sentencepiece \
+ --sacrebleu \
+ --results-path $outdir
+}
+
+
+#call_basic_model "en" "de"
+call_basic_model_smalltest "en" "de"
